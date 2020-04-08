0c78556bea884c486ed9649bb55370ed75d3901b
HDR2RD <djmd1201@gmail.com>
2020-04-09 00:20:39 +0900

    스프링부트와 AWS로 혼자 구현하는 웹서비스 챕터2

---
+++ src/main/java/com/hdr2rd/test/springboot/Application.java
@@ -0,0 +1,12 @@
+package com.hdr2rd.test.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.
+        SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static  void main(String [] args){
+        SpringApplication.run(Application.class, args);
+    }
+}
